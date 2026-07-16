675d6666f0d4044b3510479f6e239626039beb3d
JoseLH23 <chepo23larios@gmail.com>
2026-07-16 01:08:56 -0600

    Integrar textos del flujo de reservación

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,14 +4,28 @@ import LanguageDetector from "i18next-browser-languagedetector";
 
 import es from "./es.json";
 import en from "./en.json";
+import reservaUxEs from "./reservaUx.es.json";
+import reservaUxEn from "./reservaUx.en.json";
+
+const traduccionEs = {
+  ...es,
+  reservar: { ...es.reservar, ...reservaUxEs.reservar },
+  confirmacion: { ...es.confirmacion, ...reservaUxEs.confirmacion },
+};
+
+const traduccionEn = {
+  ...en,
+  reservar: { ...en.reservar, ...reservaUxEn.reservar },
+  confirmacion: { ...en.confirmacion, ...reservaUxEn.confirmacion },
+};
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
-      es: { translation: es },
-      en: { translation: en },
+      es: { translation: traduccionEs },
+      en: { translation: traduccionEn },
     },
     fallbackLng: "es",
     supportedLngs: ["es", "en"],
